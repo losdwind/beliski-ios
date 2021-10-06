7ca586280e13e5df519a74421b57d62203d83db0
Aijie Shu <s524256521@gmail.com>
2021-10-06 12:33:15 +0800

    Initial Commit

---
+++ Shared/BeliskiApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeliskiApp.swift
+//  Shared
+//
+//  Created by Losd wind on 2021/10/6.
+//
+
+import SwiftUI
+
+@main
+struct BeliskiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
